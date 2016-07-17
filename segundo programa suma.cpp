ade6be7f4f6cd057937c624f9f83353905448d02
jesusgalvis <galvis.1985@gmail.com>
2016-07-17 12:19:23 -0400

    Add files via upload

---
+++ segundo programa suma.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (){
+	int a,b;
+	printf("ingrese dos numeros separados por enter: ");
+	scanf("%d %d",&a,&b);
+	printf("la Suma de %d mas %d es %d",a,b,a+b);
+	return 0;
+}
